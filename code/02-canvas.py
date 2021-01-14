72aadce3f9eb066f0e3b410e6a7923365969e415
Bob Kozdemba <bkozdemba@gmail.com>
2021-01-13 22:17:30 -0600

    Canvas updates

---
+++ code/02-canvas.py
@@ -0,0 +1,44 @@
+from Point import Point
+from Vector import *
+from Canvas import *
+
+class Projectile:
+    def __init__(self, position, velocity):
+        self.position = position
+        self.velocity = velocity
+
+class Environment:
+    def __init__(self, gravity, wind):
+        self.gravity = gravity
+        self.wind = wind
+
+def tick(env, proj):
+    position = proj.position + proj.velocity
+    velocity = proj.velocity + env.gravity + env.wind
+    return Projectile(position, velocity)
+
+# def pos_to_pixel(width, height, x, y):
+
+# Canvas
+width = 512
+height = 512
+canvas = Canvas(width, height)
+
+# Set initial projectile position and direction.
+start = Point(0, 1, 0)
+
+initial_velocity = normalize(Vector(1, 1.8, 0)) * 7.5
+
+p = Projectile(start, initial_velocity)
+# Set environment gravity and wind vectors.
+e = Environment(Vector(0, -0.1, 0), Vector(-0.01, 0, 0))
+
+step = 0
+pen = Color(1,1,1)
+while (p.position.y > 0):
+    print("Step: {0}, Position: ({1}, {2})".format(step, p.position.x, p.position.y))
+    canvas.write_pixel(round(p.position.x), height - round(p.position.y), pen)
+    p = tick(e, p)
+    step += 1
+
+canvas.canvas_to_ppm()
