e02266e8b43432736002ae1af24012b6a6eada69
Bob Kozdemba <bkozdemba@gmail.com>
2021-01-13 21:40:38 -0600

    Canvas updates

--- code/Canvas.py
+++ code/Canvas.py
@@ -21,3 +21,30 @@ class Canvas:
     def pixel_at(self, x, y):
         index = self.width * y + x
         return self.pixels[index]
+    
+    def pix_to_char(self, p):
+        c = round(p * 255)
+        if (c < 0):
+            c = 0
+        if (c > 255):
+            c = 255
+        return c
+
+    def canvas_to_ppm(self):
+        f = open('image{0}x{1}.ppm'.format(self.width, self.height), 'w')
+        f.write('P3\n')
+        f.write('{0} {1}\n'.format(self.width, self.height))
+        f.write('255\n')
+
+        count = 0
+        for y in range(self.width):
+            for x in range(self.height):
+                if (count > 5):
+                    f.write('\n')
+                    count = 0
+                index = self.width * y + x
+                f.write('{0} {1} {2} '.format(self.pix_to_char(self.pixels[index].red), \
+                    self.pix_to_char(self.pixels[index].green), \
+                        self.pix_to_char(self.pixels[index].blue)))
+                count += 1
+        f.close()
--- hacking/make-ppm.py
+++ hacking/make-ppm.py
@@ -1,18 +1,21 @@
 #
 # Make an ascii ppm image file for testing.
 #
+width = 16
+height = 16
 f = open('tmp.ppm', 'w')
 f.write('P3\n')
-f.write('128 128\n')
+f.write('{0} {1}\n'.format(width, height))
 f.write('255\n')
 
+color = (128, 128, 128)
 pixels = 0
-for y in range(128):
-    for x in range(128):
+for y in range(width):
+    for x in range(height):
         if (pixels > 5):
             f.write('\n')
             pixels = 0
-        f.write('128 128 128 ')
+        f.write('{0} {1} {2} '.format(color[0], color[1], color[2]))
         pixels += 1
 
 f.close()
