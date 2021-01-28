8a2c01fc955b0d5879dac19b8ae70d733e3f9afb
Bob Kozdemba <bkozdemba@gmail.com>
2021-01-27 22:58:07 -0600

    inversions work

--- code/features/steps/matrices.py
+++ code/features/steps/matrices.py
@@ -234,18 +234,61 @@ def step_impl(context):
 @then(u'BINV[3,2] = -160/532')
 def step_impl(context):
     assert(math.isclose(context.BINV._[3][2], -160/532)) 
-    #raise NotImplementedError(u'STEP: Then BINV[3,2] = -160/532')
 @then(u'cofactor(AINV, 3, 2) = 105')
 def step_impl(context):
     assert(math.isclose(context.AINV.cofactor(3, 2), 105))
-    #raise NotImplementedError(u'STEP: Then cofactor(AINV, 3, 2) = 105')
 @then(u'BINV[2,3] = 105/532')
 def step_impl(context):
     assert(math.isclose(context.BINV._[2][3], 105/532)) 
-    #raise NotImplementedError(u'STEP: Then BINV[2,3] = 105/532')
 @then(u'BINV is the following 4x4 matrix')
 def step_impl(context):
     assert(context.BINV == Matrix([[ 0.21805,  0.45113,  0.24060 , -0.04511],\
         [-0.80827, -1.45677, -0.44361,  0.52068],\
             [-0.07895, -0.22368, -0.05263,  0.19737],\
                 [-0.52256, -0.81391, -0.30075,  0.30639]]))
+
+@given(u'the following 4x4 matrix invA')
+def step_impl(context):
+    context.invA = Matrix([[ 8. , -5. ,  9. ,  2. ],
+       [ 7. ,  5. ,  6. ,  1. ],
+       [-6. ,  0. ,  9. ,  6. ],
+       [-3. ,  0. , -9. , -4]])
+@then(u'inverse(A) is the following 4x4 matrix')
+def step_impl(context):
+    assert(context.invA.inverse() == Matrix([[-0.15385, -0.15385, -0.28205, -0.53846],
+       [-0.07692,  0.12308,  0.02564,  0.03077],
+       [ 0.35897,  0.35897,  0.4359 ,  0.92308],
+       [-0.69231, -0.69231, -0.76923, -1.92308]]))
+
+@given(u'the following 4x4 matrix invA3')
+def step_impl(context):
+    context.invA3 = Matrix([[ 9.,  3.,  0.,  9.],
+       [-5., -2., -6., -3.],
+       [-4.,  9.,  6.,  4.],
+       [-7.,  6.,  6.,  2.]])
+@then(u'inverse(A3) is the following 4x4 matrix')
+def step_impl(context):
+    assert(context.invA3.inverse() == Matrix([[-0.04074, -0.07778,  0.14444, -0.22222],
+       [-0.07778,  0.03333,  0.36667, -0.33333],
+       [-0.02901, -0.1463 , -0.10926,  0.12963],
+       [ 0.17778,  0.06667, -0.26667,  0.33333]]))
+
+@given(u'the following 4x4 matrix invA4')
+def step_impl(context):
+    context.invA4 = Matrix([[ 3., -9.,  7.,  3.],
+       [ 3., -8.,  2., -9.],
+       [-4.,  4.,  4.,  1.],
+       [-6.,  5., -1.,  1.]])
+@given(u'the following 4x4 matrix invB4')
+def step_impl(context):
+    context.invB4 = Matrix([[ 8.,  2.,  2.,  2.],
+       [ 3., -1.,  7.,  0.],
+       [ 7.,  0.,  5.,  4.],
+       [ 6., -2.,  0.,  5.]])
+@given(u'invC4 ← invA4 * invB4')
+def step_impl(context):
+    context.invC4 = context.invA4 * context.invB4
+@then(u'invC4 * inverse(invB4) = invA4')
+def step_impl(context):
+    assert(context.invC4 * context.invB4.inverse() == context.invA4)    
+    #raise NotImplementedError(u'STEP: Then invC4 * inverse(invB4) = invA4')
