04bfeb73d08deb0c26d97b20caf57b69c95fd4a5
Bob Kozdemba <bkozdemba@gmail.com>
2021-01-27 22:59:18 -0600

    cleaned up bugs

--- hacking/build-matrix.py
+++ hacking/build-matrix.py
@@ -3,28 +3,22 @@
 #
 # Example:
 #
-# echo "|  8 | -5 |  9 |  2 |
-#       |  7 |  5 |  6 |  1 |
-#       | -6 |  0 |  9 |  6 |
-#       | -3 |  0 | -9 | -4 |" | python build-matrix.py
+# echo "  8  -5   9   2 
+#         7   5   6   1 
+#        -6   0   9   6 
+#        -3   0  -9  -4 "  python build-matrix.py
 
-import sys
+import numpy
 
-#f=open('input.txt')
+f=open('input.txt')
 
 matrix=[]
-vector=[]
 
-#lines = f.readlines()
-lines = sys.stdin.readlines()
+lines = f.readlines()
 for line in lines:
-  vector=[]
-  for c in line:
-    if(c.isnumeric()):
-      vector.append(int(c))
-  matrix.append(vector)
+    matrix.append(line.split())
 
-print('Matrix({0})'.format(matrix))
+print(repr(numpy.array(matrix, dtype=float)))
 
 
 
